39dd36910434ef771ae7810074d8877307ba4a06
Xinxin Peng <72983482+Xinxinatg@users.noreply.github.com>
2021-07-08 00:20:34 +1000

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+class ContrastiveLoss(torch.nn.Module):
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        # euclidean_distance: [128]
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +     # calmp夹断用法
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))     
+        
+        return loss_contrastive
